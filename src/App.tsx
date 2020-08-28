703d657e86db11857701e515f70ab33209afb8a5
Kevin M Ashton <kevin.m.ashton@gmail.com>
2020-08-28 11:34:01 -0600

    Playing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function generateRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function App() {
+  const [vals, setVals] = useState<{ top: number; left: number; id: string }[]>(
+    []
+  );
+
+  const generateValues = () => {
+    let x: { top: number; left: number; id: string }[] = [];
+    for (let i = 0; i < 100; i++) {
+      x.push({
+        id: `${i}`,
+        left: generateRandomInt(0, 100),
+        top: generateRandomInt(0, 100),
+      });
+    }
+
+    setVals(x);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100%", width: "100%", position: "absolute" }}>
+      <button
+        style={{ position: "absolute", right: 5, top: 5, zIndex: 1000 }}
+        onClick={generateValues}
+      >
+        Click
+      </button>
+
+      {vals.map((v) => {
+        let nu = generateRandomInt(0, 2);
+        let color = "lime";
+        if (nu === 0) {
+          color = "pink";
+        } else if (nu === 1) {
+          color = "yellow";
+        }
+        return (
+          <div
+            key={v.id}
+            style={{
+              transition: "all 0.5s ease",
+              borderWidth: 1,
+              borderColor: "green",
+              background: color,
+              padding: 5,
+              position: "absolute",
+              left: `${v.left}%`,
+              top: `${v.top}%`,
+            }}
+          >
+            VAL {v.id}
+          </div>
+        );
+      })}
     </div>
   );
 }
